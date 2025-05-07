ac3d3e47c8eb931e7fcf03be969d9695181611e6
Stephanie Jeter <steph@stephjeter.com>
2025-05-07 18:56:05 -0400

    Added credits list as a carousel on the person details page

--- src/pages/PeopleDetails.js
+++ src/pages/PeopleDetails.js
@@ -2,6 +2,12 @@
 import { Link } from "react-router-dom";
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "../components/css/PeopleDetails.css";
 import axios from "axios";
 
 const PersonDetails = () => {
@@ -49,40 +55,53 @@ const PersonDetails = () => {
           {person.birthday && <p><strong>Born:</strong> {person.birthday}</p>}
           {person.place_of_birth && <p><strong>Place of Birth:</strong> {person.place_of_birth}</p>}
           {person.biography && <p className="mt-3">{person.biography}</p>}
+          <h3 className="mt-5 mb-3">Known For</h3>
+      <Swiper
+        modules={[Navigation, Pagination]}
+        spaceBetween={20}
+        slidesPerView={2}
+        navigation
+        pagination={{ clickable: true }}
+        breakpoints={{
+          768: { slidesPerView: 3 },
+          992: { slidesPerView: 4 },
+        }}
+      >
+      {credits.map((credit) => (
+         <SwiperSlide key={credit.id}>
+            <Link
+              to={
+                credit.media_type === "movie"
+                  ? `/movies/${credit.id}`
+                  : `/shows/${credit.id}`
+              }
+              style={{ textDecoration: "none", color: "inherit" }}
+            >
+              <div className="credits-card">
+                <img
+                  src={
+                    credit.poster_path
+                      ? `https://image.tmdb.org/t/p/w500${credit.poster_path}`
+                      : "https://via.placeholder.com/500x750?text=No+Image"
+                  }
+                  alt={credit.title || credit.name}
+                  className="img-fluid rounded mb-2"
+                />
+                <div className="credits-info">
+                  <h6>{credit.title || credit.name}</h6>
+                  <small>{credit.media_type === "movie" ? "Movie" : "TV"}</small>
+                </div>
+              </div>
+            </Link>
+          </SwiperSlide>
+         ))}
+         </Swiper>
         </div>
       </div>
 
       {/* Known For Section */}
-      <h3 className="mt-5 mb-3">Known For</h3>
-      <div className="row">
-      {credits.map((credit) => (
-        <div className="col-6 col-md-3 mb-4" key={credit.id}>
-            <Link
-            to={
-                credit.media_type === "movie"
-                ? `/movies/${credit.id}`
-                : `/shows/${credit.id}`
-            }
-            style={{ textDecoration: "none", color: "inherit" }}
-            >
-            <div className="bg-dark rounded p-2 h-100 text-center">
-                <img
-                src={
-                    credit.poster_path
-                    ? `https://image.tmdb.org/t/p/w500${credit.poster_path}`
-                    : "https://via.placeholder.com/500x750?text=No+Image"
-                }
-                alt={credit.title || credit.name}
-                className="img-fluid rounded mb-2"
-                />
-                <h6>{credit.title || credit.name}</h6>
-                <small>{credit.media_type === "movie" ? "Movie" : "TV"}</small>
-            </div>
-            </Link>
-        </div>
-        ))}
-      </div>
-    </div>
+
+   </div>
   );
 };
 
